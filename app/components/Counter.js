f27db1ba4ee776cb9daf99157cb5f66685ef7401
nischay <nischey.me008@gmail.com>
2024-08-08 23:19:34 +0530

    Added on 08 August 2024

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -4,5 +4,6 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, SetCount] = useState(0);
+  const [newCount, SetNewCount] = useState(0);
   return <button onClick={() => SetCount((c) => c + 1)}>{count}</button>;
 }
