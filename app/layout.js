5cbd5aee7c2ece4f235ccc7ed4a7a69ae18b3753
nischay <nischey.me008@gmail.com>
2024-08-14 03:01:34 +0530

    Added on 14 August 2024

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,16 @@
+import Header from "./components/Header";
 import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
 import "@/app/_style/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin);
 
 export const metadata = {
   title: {
@@ -14,13 +24,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 relative">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
