e2c60ed01e542b4c9358933243ba3fe2a9821573
nischay <nischey.me008@gmail.com>
2024-08-18 01:55:29 +0530

    added on 18th August 2024

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,6 @@
-/* eslint-disable react/no-unescaped-entities */
-import { getCabins } from "../_lib/data-service";
-import CabinCard from "../components/CabinCard";
-import Counter from "./Counter";
+import { Suspense } from "react";
+import CabinList from "../components/CabinList";
+import Spinner from "../components/Spinner";
 
 export const metadata = {
   title: "Cabins",
@@ -9,8 +8,6 @@ export const metadata = {
 
 export default async function Page() {
   // CHANGE
-  const cabins = await getCabins();
-  console.log(cabins);
 
   return (
     <div>
@@ -25,14 +22,9 @@ export default async function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
