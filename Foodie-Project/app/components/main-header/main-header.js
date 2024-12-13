c59946aa93f87dab43d0893b43ebd0c1536c0558
Nischay Sinha <nischey.me008@gmail.com>
2024-12-13 13:20:45 +0530

    added on 13 december 2024

--- Foodie-Project/app/components/main-header/main-header.js
+++ Foodie-Project/app/components/main-header/main-header.js
@@ -1,13 +1,17 @@
+
 import Link from "next/link";
-
 import LogoImg from "@/assets/logo.png";
-
 import classes from "./main-header.module.css";
-
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
+
+
+
 
 export default function MainHeader() {
+
   return (
     <>
       <MainHeaderBackground />
@@ -19,10 +23,14 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">
+                Browse Meals
+              </NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">
+                Foodies Community
+              </NavLink>
             </li>
           </ul>
         </nav>
