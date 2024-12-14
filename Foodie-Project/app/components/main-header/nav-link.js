f07af5e4721bbf6dfa74f23831ddfd2f45663284
Nischay Sinha <nischey.me008@gmail.com>
2024-12-14 18:54:20 +0530

    Added on 14 December 2024

--- Foodie-Project/app/components/main-header/nav-link.js
+++ Foodie-Project/app/components/main-header/nav-link.js
@@ -10,7 +10,6 @@ export default function NavLink({href,children}){
 
     const path=usePathname();
     return (
-<Link href={href} className={path.startsWith(href)? classes.active: undefined}>{children}</Link>
-
+<Link href={href} className={path.startsWith(href)? `${classes.active}`: undefined}>{children}</Link>
     )
 }
