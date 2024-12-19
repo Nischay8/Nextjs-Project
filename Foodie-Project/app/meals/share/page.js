f791c515c85af5b6d6be5d3b49614a2c5c4801b6
Nischay Sinha <nischey.me008@gmail.com>
2024-12-19 16:09:36 +0530

    added on 19 december

--- Foodie-Project/app/meals/share/page.js
+++ Foodie-Project/app/meals/share/page.js
@@ -1,13 +1,14 @@
+'use client';
 
+import { useFormState } from 'react-dom';
 
-import ImagePicker from '@/app/components/meals/image-picker';
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
-import { ShareMeal } from '@/lib/action';
-import MealsFormSubmit from '@/app/components/meals/meal-form-submit';
 export default function ShareMealPage() {
-
-  
+  const [state, formAction] = useFormState(shareMeal, { message: null });
 
   return (
     <>
@@ -18,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={ShareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -46,9 +47,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="your image" name="image"/>
+          <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-           <MealsFormSubmit />
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
