030e735f4414311e7f751f8506de3d5570be69d1
Nischay Sinha <nischey.me008@gmail.com>
2024-12-25 00:53:47 +0530

    Added on 25 December 2024\

--- 01-starting-project/app/header.js
+++
@@ -1,9 +0,0 @@
-export default function Header(){
-    return (
-        <>
-        <img src="/logo.png" alt="A server Surrounded by magix Sparkles"/>
-        <h1>Welcome to this NextJS Course!</h1>
-        
-        </>
-    )
-}
