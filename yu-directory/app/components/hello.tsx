bec399e8dc77718760d49fc68bfd4d70a123c97e
Nischay Sinha <nischey.me008@gmail.com>
2024-12-25 23:47:04 +0530

    added on 25 December 2024

---
+++ yu-directory/app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+function Hello(){
+    console.log("I am a Client Components!");
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+};
+
+export default Hello;
