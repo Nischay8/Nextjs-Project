97b57191bc7e54a7f2f01f1a83b59d67e6e4ceeb
Nischay Sinha <nischey.me008@gmail.com>
2025-01-06 15:50:41 +0530

    added on 06 jan 2024

--- yc_directory/app/components/StartupCard.tsx
+++ yc_directory/app/components/StartupCard.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { formatDate } from '../lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Link from 'next/link'
+import Image from 'next/image'
 const StartupCard = ({posts}:any) => {
   return (
    <li className='startup-card group'>
@@ -28,6 +29,10 @@ const StartupCard = ({posts}:any) => {
     <h3 className='text-26-semibold line-clamp-1'>{posts.title}</h3>
     </Link>
     </div>
+
+    <Link href={`/users/${posts.author?._id}`}>
+    <Image src="https://placehold.co/600X400" alt='placeholder' width={48} height={48} className='rounded-full'/>
+    </Link>
     </div>
 
    </li>
